177838260204b86a45ebbe88ac4645e051e7cce5
lemavos <lemxvos@proton.me>
2026-04-17 15:56:25 -0300

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   root: path.resolve(__dirname, "client"),
-  publicDir: false,
+  publicDir: path.resolve(__dirname, "client/public"),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
