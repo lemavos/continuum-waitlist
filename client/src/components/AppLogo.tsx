b63ce3a44ecc2b52688c0e41c503e818b41dea04
lemavos <lemxvos@proton.me>
2026-04-17 15:52:48 -0300

    v1

--- client/src/components/AppLogo.tsx
+++ client/src/components/AppLogo.tsx
@@ -5,7 +5,7 @@ interface AppLogoProps {
 export default function AppLogo({ className = "w-7 h-7" }: AppLogoProps) {
   return (
     <img
-      src="/favicon.ico"
+      src="client/src/public/favicon.ico"
       alt="Continuum logo"
       className={className}
       aria-hidden="true"
